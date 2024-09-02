d5b93c0ee85802283247dcc5fb9eebf1d7f296be
Vasu Bhatia <vasubhatia991@gmail.com>
2024-09-02 13:22:02 +0530

    testing added

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator functions
+"""
+def add(x, y):
+    """Add x and y and return result"""
+    return x + y
+
+def substract(x, y):
+    """Substract x form y"""
+    return abs(x - y)
